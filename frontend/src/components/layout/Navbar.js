f7c6721d5ae8c52eda71f09089819e33b695c2ae
ElevenFL <86896084+ElevenFL@users.noreply.github.com>
2025-06-11 15:51:49 -0300

    Arreglos al merge

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -15,7 +15,6 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                <img src="https://sdmntpreastus.oaiusercontent.com/files/00000000-3864-61f9-8d2a-b0cdbac6b231/raw?se=2025-06-11T06%3A44%3A45Z&sp=r&sv=2024-08-04&sr=b&scid=b7e5945d-9da3-5587-84ce-803dddeda15a&skoid=31bc9c1a-c7e0-460a-8671-bf4a3c419305&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-10T11%3A46%3A23Z&ske=2025-06-11T11%3A46%3A23Z&sks=b&skv=2024-08-04&sig=uA/oWvTPxT%2BPH/kMpBud55%2BOHrjFHy98123wSLlNQpg%3D" alt="Logo" className="logo" />
                 <Link to="/">60FPS</Link>
             </div>
 

d421b590e7c6183760a28bc1c8f0864d7c62054c
Patricio Rene <patorjuarezstordeur@gmail.com>
2025-06-18 01:01:58 -0300

    Cambios generales

--- microservices/auth-service/src/models/user.model.js
+++ microservices/auth-service/src/models/user.model.js
@@ -27,7 +27,6 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// Método para encriptar contraseña antes de guardar
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
     
@@ -40,7 +39,6 @@ userSchema.pre('save', async function(next) {
     }
 });
 
-// Método para comparar contraseñas
 userSchema.methods.comparePassword = async function(candidatePassword) {
     return bcrypt.compare(candidatePassword, this.password);
 };
